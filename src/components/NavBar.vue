6aeda54a5907dc4e93430ca3c092a3a9ad56fb45
luc-dumas <112646178+luc-dumas@users.noreply.github.com>
2024-03-26 21:20:05 -0400

    Added tufte css for text styles

---
+++ src/components/NavBar.vue
@@ -0,0 +1,60 @@
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+  <RouterView />
+</template>
+
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+  name: 'NavBar',
+  components: {
+  },
+  props: {
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  },
+  async created() {
+  },
+};
+</script>
+
+<style>
+  nav {
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+    margin-top: 2rem;
+  }
+
+  nav a.router-link-exact-active {
+    color: var(--color-text);
+  }
+
+  nav a.router-link-exact-active:hover {
+    background-color: transparent;
+  }
+
+  nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+  }
+
+  nav a:first-of-type {
+    border: 0;
+  }
+
+  @media (min-width: 1024px) {
+  }
+
+</style>
