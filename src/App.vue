b32dab69c4d17807d247f4ee7c3636abe3cdd9b8
luc-dumas <112646178+luc-dumas@users.noreply.github.com>
2024-04-06 11:58:12 -0400

    Added transition effects, pages, router links

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,46 @@ import NavBar from './components/NavBar.vue'
 
 <template>
   <div class="whole-page">
-    <NavBar class="nav-bar"/>
-    <RouterView />
+    <!-- <Transition> -->
+      <NavBar class="nav-bar"/>
+    <!-- </Transition> -->
+    <Transition>
+      <RouterView />
+    </Transition>
   </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  data() {
+    return {
+      // show: false
+    };
+  },
+  methods: {
+    // toggle() {
+    //   this.show = !this.show;
+    // }
+  },
+  mounted() {
+    // setTimeout(() => {
+    //   this.toggle();
+    // }, 500);
+  },
+};
+</script>
 
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.75s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
 
   .whole-page {
   }
